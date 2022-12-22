6e21c1a8cc8c09f40c87767a1077bc89a70373e9
Yuri Bekmetov <bekm.yu@yandex.ru>
2022-12-22 15:34:37 +0200

    login form updated

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -21,15 +21,15 @@ router.post("/login", async (req, res) => {
 
     if (!user) {
         res.json({ error: "User Doesn't Exist" });
-    }
-
-    bcrypt.compare(password, user.password).then((match) => {
-        if (!match) {
-            res.json({ error: "Wrong Username And Password"});
-        } else {
-            res.json("You logged in!")
-        }
-    })
+    } else {
+        bcrypt.compare(password, user.password).then((match) => {
+            if (!match) {
+                res.json({ error: "Wrong Username And Password"});
+            } else {
+                res.json("You logged in!")
+            }
+        });
+    };
 });
 
 module.exports = router;
