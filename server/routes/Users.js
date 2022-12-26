6c2f3934d100b7af0fb01dedeb93e9688a0a7017
Yuri Bekmetov <bekm.yu@yandex.ru>
2022-12-26 09:41:27 +0200

    Login Auth added

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
+const {validateToken} = require("../middlewares/AuthMiddleware")
 
 const {sign} = require("jsonwebtoken");
 
@@ -36,5 +37,9 @@ router.post("/login", async (req, res) => {
         });
     };
 });
+
+router.get("/auth", validateToken, (req, res) => {
+    res.json(req.user);
+})
 
 module.exports = router;
